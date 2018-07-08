ed15b9a69b119fef6352a0cf4855bd61809a808b
Ashwin Dhakal <ashwindhakal97@gmail.com>
2018-07-08 11:49:23 +0545

    new setup previous

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "majorproject.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sajilo_hostel.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
